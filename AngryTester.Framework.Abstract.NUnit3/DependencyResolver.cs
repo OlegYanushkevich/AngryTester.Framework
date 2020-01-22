6019bc36f0154a905c15e386ba98de0e28ac8b19
Aleh Yanushkevich2 <Aleh_Yanushkevich2@epam.com>
2020-01-22 12:11:04 +0300

    commit

--- AngryTester.Framework.Abstract.NUnit3/DependencyResolver.cs
+++ AngryTester.Framework.Abstract.NUnit3/DependencyResolver.cs
@@ -1,6 +1,8 @@
 ﻿using AngryTester.Framework.Abstract.NUnit3.Exceptions;
 using Autofac;
 using Autofac.Core;
+using System;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 
 namespace AngryTester.Framework.Abstract.NUnit3
@@ -11,14 +13,29 @@ namespace AngryTester.Framework.Abstract.NUnit3
 
 		private static readonly ContainerBuilder builder = new ContainerBuilder();
 
-		public static void ApplyModule<TModule>() where TModule : class, IModule, new()
+		public static void RegisterAssemblyModules(Assembly[] assemblies)
 		{
 			if (container != null)
 			{
 				throw new ContainerAlreadyInitializedException("Container is already initialized. Applying module after container initialization is impossible.");
 			}
 
-			builder.RegisterModule<TModule>();
+			builder.RegisterAssemblyModules(assemblies);
+		}
+
+		public static void ApplyModule(IModule module)
+		{
+			if (container != null)
+			{
+				throw new ContainerAlreadyInitializedException("Container is already initialized. Applying module after container initialization is impossible.");
+			}
+
+			builder.RegisterModule(module);
+		}
+
+		public static void ApplyModule<TModule>() where TModule : class, IModule, new()
+		{
+			ApplyModule(Activator.CreateInstance<TModule>());
 		}
 
 		[MethodImpl(MethodImplOptions.Synchronized)]
---
+++ AngryTester.Framework.Abstract.Web/Attributes/RegisterModulesAttribute.cs
@@ -0,0 +1,31 @@
+﻿using AngryTester.Framework.Abstract.NUnit3;
+using Autofac.Core;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using NUnit.Framework.Internal;
+using System;
+
+namespace AngryTester.Framework.Abstract.Web.Attributes
+{
+	[AttributeUsage(AttributeTargets.Assembly)]
+	public class RegisterModulesAttribute : NUnitAttribute, IApplyToContext
+	{
+		private readonly Type[] types;
+
+		public RegisterModulesAttribute(params Type[] types)
+		{
+			this.types = types;
+		}
+
+		public void ApplyToContext(TestExecutionContext context)
+		{
+			foreach (var type in types)
+			{
+				if (Activator.CreateInstance(type) is IModule module)
+				{
+					DependencyResolver.ApplyModule(module);
+				}
+			}
+		}
+	}
+}
